a877fe70869f449050f3463aad71f836734334f2
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-14 12:46:48 -0300

    feat: add new functions on header

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,10 +6,28 @@ import ThemeToggle from "./ThemeToggle";
 import Logo from './Logo';
 import Nav from './Nav';
 import MobileNav from './MobileNav';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+    const [header, setHeader] = useState(false);
+    const pathname = usePathname();
+    
+    useEffect(()=> {
+        const scrollYPos = window.addEventListener('scroll', ()=> {
+            window.scrollY > 50 ? setHeader(true) : setHeader(false);
+        });
+
+        //remove event 
+        return () => window.removeEventListener('scroll', scrollYPos);
+    })
+
     return (
-        <header>
+        <header
+            className={`${
+                header
+                    ? 'py-5 bg-white shadow-lg dark:bg-accent'
+                    : 'py-6 dark:bg-transparent'
+                } sticky top-0 z-30 transition-all ${pathname === '/' && 'bg-[#fef9f5]'}`}>
             <div className="container mx-auto">
                 <div className='flex justify-between items-center'>
                     <Logo />
