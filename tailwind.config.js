2e862de740605640f0242ff827365683057b3297
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-14 16:31:01 -0300

    feat: add new styling configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,6 +76,9 @@ module.exports = {
       },
       backgroundImage: {
         hero: 'url(/hero/hero-bg.png)',
+        hero_shape: 'url(/hero/shape-1.svg)',
+        hero_shape2_light: 'url(/hero/shape-2-light.svg)',
+        hero_shape2_dark: 'url(/hero/shape-2-dark.svg)',
       },
     },
   },
