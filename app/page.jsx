772a9547bbe2de867ea116c263e5aec618769c5d
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-14 19:55:59 -0300

    feat: add Services component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Work from "@/components/Work";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Services />
+      <Work />
     </main>
   );
 }
