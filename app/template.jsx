2b9ec19306fc476ba61239cff75009eb268c2250
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-14 11:50:01 -0300

    feat: add navigation bar, link, functions and animations

--- app/template.jsx
+++ app/template.jsx
@@ -18,7 +18,7 @@ const Template = ({children}) => {
             <motion.main variants={variants} initial='hidden' animate='enter' transition={{type: 'linear', delay: 0.2, duration:0.4}}>
                 {children}
             </motion.main>
-            /* completion bar */
+            {/* completion bar */}
             <span style={{transform: `translateY(${completion - 100}%)`}} className='fixed z-50 bg-primary w-1 top-0 right-0 bottom-0 transition-all duration-700'></span>
             <div className='h-[4000px]'></div>
         </>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,33 @@
+'use client'
+
+import React, {useState, useEffect } from 'react';
+//components
 import ThemeToggle from "./ThemeToggle";
+import Logo from './Logo';
+import Nav from './Nav';
+import MobileNav from './MobileNav';
 
 const Header = () => {
     return (
         <header>
-            <ThemeToggle />
+            <div className="container mx-auto">
+                <div className='flex justify-between items-center'>
+                    <Logo />
+                    <div className='flex items-center gap-x-6'>
+                        {/* Nav */}
+                        <Nav
+                            containerStyles={'hidden xl:flex gap-x-8 items-center'}
+                            linkStyles="relative hover:text-primary transition-all"
+                            underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+                        />
+                        <ThemeToggle />
+                        {/* Mobile Nav */}
+                        <div className='xl:hidden'>
+                            <MobileNav />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
