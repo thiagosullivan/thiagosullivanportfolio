acf99a3cf3366ad0bf03f42adaccc732831b053d
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-16 03:24:50 -0300

    feat: add Project page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,131 @@
+'use client';
+
+import { useState } from "react";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+import ProjectCard from "@/components/ProjectCard";
+
+const ProjectData = [
+    {
+        image: '/work/3.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/4.png',
+        category: 'react js',
+        name: 'Solstice Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/2.png',
+        category: 'react js',
+        name: 'Evolve Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/1.png',
+        category: 'next js',
+        name: 'Ignite Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/3.png',
+        category: 'next js',
+        name: 'Envision Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/1.png',
+        category: 'next js',
+        name: 'Nova Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/4.png',
+        category: 'fullstack',
+        name: 'Zenith Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/2.png',
+        category: 'fullstack',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, non.',
+        link: '/',
+        github: '/',
+    },
+];
+
+// remove category duplicates
+const uniqueCategories = [
+    'all projects',
+    ...new Set(ProjectData.map(item => item.category)),
+];
+
 const Projects = () => {
+    const [categories, setCategories] = useState(uniqueCategories);
+    const [category, setCategory] = useState('all projects')
+
+    const filteredProjects = ProjectData.filter(project => {
+        // if category is 'all projects' return all Projects, else filter by category
+
+        return category === 'all projects'
+        ? project
+        : project.category === category;
+    })
+
+    console.log(filteredProjects)
+
     return (
-        <div>Projetos</div>
+        <section className="min-h-screen pt-12">
+            <div className="container mx-auto">
+                <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">My Projects</h2>
+                {/* tabs */}
+
+                <Tabs defaultValue={category} className="mb-24 xl:mb-48">
+                    <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
+                        {categories.map((category, index) => {
+                            return (
+                                <TabsTrigger
+                                    onClick={() => setCategory(category)}
+                                    value={category}
+                                    key={index}
+                                    className="capitalize w-[162px] md:w-auto"
+                                >
+                                    {category}
+                                </TabsTrigger>
+                            )
+                        })}
+                    </TabsList>
+                    {/* tabs content */}
+                    <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+                        {filteredProjects.map((project, index) => {
+                            return (
+                                <TabsContent value={category} key={index}>
+                                    <ProjectCard project={project} />
+                                </TabsContent>
+                            )
+                        })}
+                    </div>
+                </Tabs>
+            </div>
+        </section>
     );
 }
  
