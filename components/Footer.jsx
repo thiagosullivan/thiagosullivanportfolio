9851ce5027ce29b91de6256ec9cc9dc4124d8707
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-28 16:06:49 -0300

    feat: changes on bottom footer infos

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,28 @@
 import Socials from "./Socials";
 
-
 const Footer = () => {
-    return (
-        <footer className="bg-secondary py-12">
-            <div className="container mx-auto">
-                <div className="flex flex-col items-center justify-between">
-                    {/* socials */}
-                    <Socials containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4" iconsStyles="text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all" />
-                    {/* copyright */}
-                    <div className="text-white">
-                        Copyright &copy; Thiago "Sullivan" Santos. All rights reserved.
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
-}
- 
+  const currentTime = new Date();
+
+  const year = currentTime.getFullYear();
+
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          {/* socials */}
+          <Socials
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+            iconsStyles="text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all"
+          />
+          {/* copyright */}
+          <div className="text-white">
+            Developed by Thiago "Sullivan" Santos | Copyright &copy; {year}. All
+            rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
