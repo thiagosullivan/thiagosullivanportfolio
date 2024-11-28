8b8c638a92ae78398dde2b4a34178a15d7c75c55
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-28 16:06:36 -0300

    feat: changes on project individual page to show image and links

--- app/projects/[slug]/page.jsx
+++ app/projects/[slug]/page.jsx
@@ -1,9 +1,12 @@
 import { fetchHygraphQuery } from "@/utils/fetch-hygraph-query";
 import { RichText } from "@/utils/rich-text";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Link2 } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const getPageData = async (slug) => {
-    const query = `
+  const query = `
         query blogPosts {
             blogPosts(where: {slug:  "${slug}"}) {
                 createdAt
@@ -13,7 +16,6 @@ const getPageData = async (slug) => {
                 postText {
                     raw
                 }
-                description
                 imagePost {
                     url
                 }
@@ -25,62 +27,90 @@ const getPageData = async (slug) => {
                 }
                 technology {
                     technologyName
-                }    
+                }
+              link
+              repo
             }
             technologies {
                 technologyName
             }
         }
-      `
-  
-    return fetchHygraphQuery(
-      query,
-        //   1000 * 60 * 60 * 24, 
-      // 1 day
-    )
-  }
-  
+      `;
 
-const PageProject = async ({ params: {slug}}) => {
+  return fetchHygraphQuery(
+    query
+    //   1000 * 60 * 60 * 24,
+    // 1 day
+  );
+};
 
-    const {blogPosts} = await getPageData(slug);
+const PageProject = async ({ params: { slug } }) => {
+  const { blogPosts } = await getPageData(slug);
 
-    console.log(blogPosts, 'PROJ PAGE IND')
-    
-    return (
-        <div className="min-h-screen pt-12">
-            <div className="container mx-auto">
-                {blogPosts.map(item => {
+  console.log(blogPosts, "PROJ PAGE IND");
+
+  return (
+    <div className="min-h-screen pt-12">
+      <div className="container mx-auto">
+        {blogPosts.map((item) => {
+          return (
+            <div key={item.id}>
+              <h1 className="text-4xl text-center font-bold before:bg-dots_light">
+                <span className="border-b-2 border-bg-primary">
+                  {item.title}
+                </span>
+              </h1>
+              <div className="bg-primary w-max py-1 px-3 rounded-lg my-7">
+                <p className="text-Secondary">{item.tec}</p>
+              </div>
+              <div>
+                <RichText content={item.postText.raw} />
+                <div className="border-b border-b-gray-300 mt-7" />
+                <div>
+                  {item.projectImages.map((itemImg, index) => {
                     return (
-                        <div key={item.id}>
-                            <h1 className="text-4xl text-center font-bold before:bg-dots_light">{item.title}</h1>
-                            <div className="bg-primary w-max py-1 px-3 rounded-lg my-7"><p className="text-Secondary">{item.tec}</p></div>
-                            <RichText
-                                content={item.postText.raw}
-                            />
-                            {/* <div>
-                                {item.projectImages.map(itemImg => {
-                                    return (
-                                        <div>
-                                            <Image
-                                                src={itemImg.url}
-                                                alt="Imagem do Projeto"
-                                                width={100}
-                                                height={100}
-                                            />
-                                        </div>
-                                    )
-                                })}
-                            </div> */}
-                        </div>
-                    )
-                })}
-
-                
+                      <div
+                        className="w-full h-[700px] relative mt-12"
+                        key={index}
+                      >
+                        <Image
+                          src={itemImg.url}
+                          alt="Imagem do Projeto"
+                          fill
+                          className="object-contain"
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="border-b border-b-gray-300 mt-7" />
+                <div className="flex justify-center items-center gap-3 mb-12 mt-7">
+                  <Link
+                    href={item.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 text-lg bg-secondary text-white px-2 py-1 rounded-md hover:bg-primary transition-all"
+                  >
+                    <Link2 size={20} />
+                    <p>Live Demo</p>
+                  </Link>
+                  <Link
+                    href={item.repo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 text-lg bg-secondary text-white px-2 py-1 rounded-md hover:bg-primary transition-all"
+                  >
+                    <GitHubLogoIcon size={20} />
+                    <p>Repo</p>
+                  </Link>
+                </div>
+              </div>
             </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    );
-}
- 
 export default PageProject;
